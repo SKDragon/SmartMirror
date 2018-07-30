e8a7b5788e96297a14df2e87754f0d7f3628a15e
Gavin <gavin.leung.cms@gmail.com>
2018-07-30 10:40:40 -0400

    Edited clock module
    formatting and added 12am

--- clock.js
+++ clock.js
@@ -1,35 +1,38 @@
 function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    //h = 13;
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
+  var today = new Date();
+  var h = today.getHours();
+  //h = 0;
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
 
-    if (checkHrPM(h)){
-    if (h>12){
-    	h = h-12;
-    }
-    	document.getElementById('txt').innerHTML =
-    	h + ":" + m + "pm";
-    }
-    else{
-    	document.getElementById('txt').innerHTML =
-    	h + ":" + m + "am";
-    }
+  if (checkHrPM(h)){
+      if (h>12){
+        h = h-12;
+      }
+  	document.getElementById('txt').innerHTML =
+  	h + ":" + m + "pm";
+  }
+  else{
+      if (h==0){
+          h = 12;
+      }
+  	document.getElementById('txt').innerHTML =
+  	h + ":" + m + "am";
+  }
 
-    var t = setTimeout(startTime, 500);
+  var t = setTimeout(startTime, 500);
 }
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
 }
 function checkHrPM(i){
-	if (i<12){
-    	return false;
-    }
-    else{
-    	return true;
-    }
+if (i<12){
+  	return false;
+  }
+  else{
+  	return true;
+  }
 }
