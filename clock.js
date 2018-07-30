222f2c11f155f4fe84e109ccd8ef898502a1a0a4
Gavin <gavin.leung.cms@gmail.com>
2018-07-30 13:21:40 -0400

    Added date to clock.js
    Added the date to clock, and prints it in index

--- clock.js
+++ clock.js
@@ -7,6 +7,8 @@ function startTime() {
   m = checkTime(m);
   s = checkTime(s);
 
+  var n = formatDate(today);
+
   if (checkHrPM(h)){
       if (h>12){
         h = h-12;
@@ -22,8 +24,16 @@ function startTime() {
   	h + ":" + m + "am";
   }
 
+  document.getElementById('date').innerHTML = n;
+  //console.log(formatDate(today));
+
+
+
+  //
   var t = setTimeout(startTime, 500);
 }
+
+
 function checkTime(i) {
   if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
   return i;
@@ -35,4 +45,22 @@ if (i<12){
   else{
   	return true;
   }
+}
+
+function formatDate(date) {
+  var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+var weekNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+];
+
+	var d = date.getDay();
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return weekNames[d] + ', ' + monthNames[monthIndex] + ' ' + day + ', '+ year;
 }
