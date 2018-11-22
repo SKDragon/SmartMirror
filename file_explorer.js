51efff2beecb278bfbb93f66b269fd308b971659
Gavin Leung <gavin.leung.cms@gmail.com>
2018-11-22 18:37:34 -0500

    update file_explorer
    Opens file explorer dialog
    Still needs way of saving chosen file url

--- file_explorer.js
+++ file_explorer.js
@@ -1,12 +1,6 @@
-const remote = require('electron');
-
-function openfile(){
-  var {dialog} = remote;
-  dialog.showOpenDialog({properties: ['openFile'], filters: [{name: 'Images', extensions:['jpg', 'png', 'jpeg']}]});
-  function(file){
-    console.log(file);
-  }
-
-
-
-}
+const {dialog} = require('electron').remote;
+const pic_url="";
+//var {dialog} = remote;
+document.getElementById('explorer').addEventListener("click", () =>{
+  dialog.showOpenDialog({properties: ['openFile'], filters: [{name: 'Images', extensions:['jpg', 'png', 'jpeg']}]}, selectedFiles => console.log(selectedFiles));
+}, false);
