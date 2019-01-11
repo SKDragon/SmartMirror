7db072d4d5072ce52b9daed4c1a81f73d1148d68
Gavin Leung <gavin.leung.cms@gmail.com>
2019-01-10 21:23:55 -0500

    File explorer working
    finally got the explorer to work by choosing a picture and displaying it

--- file_explorer.js
+++ file_explorer.js
@@ -52,9 +52,15 @@
 // 	};
 // });
 
-const pic_url="";
+var pic_url= "";
 const {dialog}= require('electron').remote;
-document.getElementById('explorer').addEventListener("click", () =>{
+document.getElementById('explorer').addEventListener("click", ()=>{
   dialog.showOpenDialog({properties: ['openFile'], filters: [{name: 'Images',
-  extensions:['jpg', 'png', 'jpeg']}]}, selectedFiles => console.log(selectedFiles));
-}, false);
+  extensions:['jpg', 'png', 'jpeg']}]}, function (files) {
+        if (files !== undefined) {
+            //alert(files[0]);
+						document.getElementById('pic').src= files[0];
+						//alert(document.getElementById('pic').getImagePath);
+        }
+			});
+		});
