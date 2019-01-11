f96b7e59c8099fdc762b6195abd8e803eafd5c3a
Gavin Leung <gavin.leung.cms@gmail.com>
2019-01-10 20:11:04 -0500

    reverted back
    reverted back to jquery window, and not angular

--- file_explorer.js
+++ file_explorer.js
@@ -6,48 +6,55 @@
 //   extensions:['jpg', 'png', 'jpeg']}]}, selectedFiles => console.log(selectedFiles));
 // }, false);
 
-const {dialog} = require('electron').remote;
-var app = angular.module('app', ['ngRoute']);
+// const {dialog} = require('electron').remote;
+// var app = angular.module('app', ['ngRoute']);
+//
+// // const {remote} = require('electron');
+//
+// app.service('image', function() {
+// 	var imagePath = "";
+// 	var dimesions = [];
+// 	this.setImagePath = function(path) {
+// 		imagePath = path;
+// 	};
+// 	this.getImagePath = function() {
+// 		return imagePath;
+// 	};
+// 	this.setImageDimensions = function(imgDimesions) {
+// 		dimesions = imgDimesions;
+// 	};
+// 	this.getImageDimensions = function() {
+// 		return dimesions;
+// 	};
+// });
+//
+// app.controller('homeCtrl', function($scope, $location, image) {
+// 	$scope.pickFile = function() {
+// 		dialog.showOpenDialog({
+// 			properties: ['openFile'],
+// 			filters: [{
+// 				name: 'Images',
+// 				extensions: ['jpg', 'jpeg', 'png']
+// 			}]
+// 		}, function(file) {
+// 			if(!!file) {
+// 				var path = file[0];
+// 				image.setImagePath(path);
+// 				var sizeof = require('image-size');
+//
+// 				var dimesions = sizeof(path); // dimesions.width and dimesions.height
+//
+// 				image.setImageDimensions(dimesions);
+// 				$location.path('/edit');
+// 				$scope.$apply();
+// 			}
+// 		});
+// 	};
+// });
 
-// const {remote} = require('electron');
-
-app.service('image', function() {
-	var imagePath = "";
-	var dimesions = [];
-	this.setImagePath = function(path) {
-		imagePath = path;
-	};
-	this.getImagePath = function() {
-		return imagePath;
-	};
-	this.setImageDimensions = function(imgDimesions) {
-		dimesions = imgDimesions;
-	};
-	this.getImageDimensions = function() {
-		return dimesions;
-	};
-});
-
-app.controller('homeCtrl', function($scope, $location, image) {
-	$scope.pickFile = function() {
-		dialog.showOpenDialog({
-			properties: ['openFile'],
-			filters: [{
-				name: 'Images',
-				extensions: ['jpg', 'jpeg', 'png']
-			}]
-		}, function(file) {
-			if(!!file) {
-				var path = file[0];
-				image.setImagePath(path);
-				var sizeof = require('image-size');
-
-				var dimesions = sizeof(path); // dimesions.width and dimesions.height
-
-				image.setImageDimensions(dimesions);
-				$location.path('/edit');
-				$scope.$apply();
-			}
-		});
-	};
-});
+const pic_url="";
+const {dialog}= require('electron').remote;
+document.getElementById('explorer').addEventListener("click", () =>{
+  dialog.showOpenDialog({properties: ['openFile'], filters: [{name: 'Images',
+  extensions:['jpg', 'png', 'jpeg']}]}, selectedFiles => console.log(selectedFiles));
+}, false);
